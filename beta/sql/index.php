369375b04f4786ec6b5adccc66340ed0d5a58ff7
KDragonM <kennethmitra@gmail.com>
2016-10-24 22:11:51 -0500

    Added php code to connect to MySQL server and secured .htaccess, .htpasswd, and .mysqlpassword files

--- beta/sql/index.php
+++ beta/sql/index.php
@@ -5,6 +5,24 @@
         <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
     </head>
     <body>
+        <?php 
         
+        $server = "localhost";
+        $user = "webserver";
+        
+        $passwordFile = fopen(".mysqlpassword", "r") or die("Unable to open password file!"); //Try to get my password now! JK please don't try
+        $pass = fgets($passwordFile);
+        close($passwordFile);
+        
+        $connection = mysqli($server,$user,$pass);
+        if(!$connection){
+            die("connection failed: ". mysqli_connect_error());
+        }else{
+            echo "Conenction Sucessful!";
+        }
+        
+        
+        
+        ?>
     </body>
 </html>
